110a52f35b0b1dedfbe1acc1282ae2712de8aa99
Jacob Budzis <jrbudzis@gmail.com>
2019-05-02 13:21:08 -0700

    Add splash screen and clean code

--- main.py
+++ main.py
@@ -2,51 +2,72 @@ import curses
 from game import Minesweeper 
 
 
+# Window Constants
+WIN_HEIGHT_OFFSET = 5
+
+# Board Constants
 NUM_ROWS = 20
 NUM_COLS = 40
 NUM_MINES = 150
+
+# Display Constants
 STR_COVERED = 'x'
 STR_MINE = '*'
+STR_FLAG = '!'
 
+# Curses Color Pair Constants
 PAIR_MINE = 10
 PAIR_FLAG = 11
+
+# Splash Screen
+LOGO = r"""
+        _                                                   
+  /\/\ (_)_ __   ___  _____      _____  ___ _ __   ___ _ __ 
+ /    \| | '_ \ / _ \/ __\ \ /\ / / _ \/ _ \ '_ \ / _ \ '__|
+/ /\/\ \ | | | |  __/\__ \\ V  V /  __/  __/ |_) |  __/ |   
+\/    \/_|_| |_|\___||___/ \_/\_/ \___|\___| .__/ \___|_|   
+                                           |_|              
+    
+                  Created by: Jacob Budzis
+                  Updated:    May 2, 2019
+
+                  Controls
+                   f: Flag a location as a mine
+                   c: Check the covered location
+                   e: Exit the game
+
+                  PRESS ANY KEY TO CONTINUE
+
+"""
 
 def isValidCoordinate(x, y):
     return ( x >= 0 ) and ( y >= 0 ) and ( x < NUM_COLS ) and ( y < NUM_ROWS ) 
 
 def revealTile(x, y, win, game):
-    if not isValidCoordinate(x, y) or win.inch(y,x) == ord(' '):
+    if not isValidCoordinate(x, y) or (win.inch(y,x) & 0xff) == ord(' '):
         return
     
     val = game.getValue(y, x)
+
+    # CASE: No mines surrounding tile. Flood reveal the board
     if val == 0:
         win.addstr(' ')
-        win.refresh()       
  
         # Recurse on all surrounding tiles
         for xi in [x-1, x, x+1]:
             for yi in [y-1, y, y+1]:
                 revealTile(xi, yi, win, game)
     
+    # CASE: Tile contains mine
     elif game.containsMine(y, x):
         win.addstr(STR_MINE, curses.color_pair(PAIR_MINE))
+    
     else:
         win.addstr(str(val), curses.color_pair(val))
     
     win.refresh()
 
-def main(stdscr):
-    # Clear the screen
-    stdscr.clear()
-    
-    # Initialize the display
-    for y in range(NUM_ROWS):
-        for x in range(NUM_COLS):
-            stdscr.addstr(y, x, STR_COVERED)
-    stdscr.move(0,0)
-    stdscr.refresh()
-
-    # Initialize colors
+def initialize_color_pairs():
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
@@ -59,9 +80,30 @@ def main(stdscr):
     curses.init_pair(PAIR_MINE, curses.COLOR_BLACK, curses.COLOR_RED)
     curses.init_pair(PAIR_FLAG, curses.COLOR_BLACK, curses.COLOR_GREEN)
 
+def main(stdscr):
+    # Clear the screen
+    stdscr.clear()
+
+    # Display the logo and wait for user input
+    stdscr.addstr(LOGO)
+    stdscr.refresh()
+    stdscr.getch()
+    
+    # Initialize the game grid display
+    stdscr.clear()
+    for y in range(NUM_ROWS):
+        for x in range(NUM_COLS):
+            stdscr.addstr(y, x, STR_COVERED)
+    stdscr.move(0,0)
+    stdscr.refresh()
+
+    # Initialize colors
+    initialize_color_pairs()
+
     # Initialize the Minesweeper game
     minesweeper = Minesweeper(NUM_ROWS, NUM_COLS, NUM_MINES)
 
+    # Run the game logic
     while True:
         c = stdscr.getch()
         (y, x) = stdscr.getyx()        
@@ -85,7 +127,7 @@ def main(stdscr):
             stdscr.move(y, x)
             stdscr.refresh()
         elif c == ord('f'):
-            stdscr.addstr('f', curses.color_pair(PAIR_FLAG))
+            stdscr.addstr(STR_FLAG, curses.color_pair(PAIR_FLAG))
 
             # Avoid moving the cursor.
             stdscr.move(y, x)
